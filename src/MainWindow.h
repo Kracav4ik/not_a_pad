fccb6c263fcf8e8cf87203e2e08291f46612fdd4
Kracav4ik <dikama2013@yandex.ru>
2016-11-02 02:30:51 +0300

    Open and save

--- src/MainWindow.h
+++ src/MainWindow.h
@@ -1,15 +1,44 @@
 #pragma once
 
 #include <QMainWindow>
+#include <QFileDialog>
 
 #include "ui_n_a_p.h"
 
-class MainWindow: public QMainWindow, private Ui::MainWindow{
-    Q_OBJECT
+class MainWindow : public QMainWindow, private Ui::MainWindow {
+Q_OBJECT
 public:
-    MainWindowa() {
+    MainWindow() {
         setupUi(this);
         show();
     }
+
+public slots:
+
+    void on_actionSave_As_triggered() {
+        QString fileName = QFileDialog::getSaveFileName(this, "Save file");
+        printf("%s\n", fileName.toUtf8().data());
+        if (fileName.isNull()) {
+            return;
+        }
+        QFile file(fileName);
+        file.open(QIODevice::Text | QIODevice::WriteOnly);
+        file.write(plainTextEdit->toPlainText().toUtf8());
+
+    }
+
+    void on_actionOpen_triggered() {
+        QString fileName = QFileDialog::getOpenFileName(this, "Open file");
+        printf("%s\n", fileName.toUtf8().data());
+        if (fileName.isNull()) {
+            return;
+        }
+        QFile file(fileName);
+        file.open(QIODevice::Text | QIODevice::ReadOnly);
+        QString str = QString::fromUtf8(file.readAll());
+        plainTextEdit->setPlainText(str);
+
+    }
+
 };
 
